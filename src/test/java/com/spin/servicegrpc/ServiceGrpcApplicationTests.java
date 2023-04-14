a4087557e83dafdb5e9352e8bd065a8c5a5ad576
Carlos Zambrano <czambrano@palo-it.com>
2023-04-14 14:23:59 -0500

    Primer commit

---
+++ src/test/java/com/spin/servicegrpc/ServiceGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spin.servicegrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
